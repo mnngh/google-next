f2267f5d19ef0632f0845bb527076cdb46617af7
nurnabi <mnn.saag@gmail.com>
2024-05-06 04:58:39 +0300

    footer created

---
+++ src/components/CountryLookup.jsx
@@ -0,0 +1,20 @@
+"use client";
+import { useState, useEffect } from "react";
+
+export default function CountryLookup() {
+  const [country, setCountry] = useState("Bangladesh");
+
+  useEffect(() => {
+    const getCountry = async () => {
+      const response = await fetch(
+        `https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.country);
+      if (!response) return;
+      setCountry(response);
+    };
+    getCountry();
+  }, []);
+  return <div>{country}</div>;
+}
